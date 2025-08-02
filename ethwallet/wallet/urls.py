7af718bfa2f2f4887c9efe39f88a1eb8bcb31480
SaadTech360 <saadibrahimumar7@gmail.com>
2025-08-02 01:25:00 +0100

    create the send transaction functon using mock addresses

---
+++ ethwallet/wallet/urls.py
@@ -0,0 +1,8 @@
+from django.urls  import path
+from .views  import create_account,get_balance,send_transaction
+
+urlpatterns = [
+    path('create',create_account,name='create'),
+    path('balance/<str:contract_address>',get_balance,name='get_balance'),
+    path('send-transaction/',send_transaction,name='send'),
+]

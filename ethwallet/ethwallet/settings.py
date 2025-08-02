b235569425d4b09f0ffb33e79b26c4fd2e68f874
SaadTech360 <saadibrahimumar7@gmail.com>
2025-08-02 15:03:06 +0100

    e

--- ethwallet/ethwallet/settings.py
+++ ethwallet/ethwallet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&8!_*770qw7$(e-hf=-i#v#tn3qp7t0slv=^84#@a=&@v#3#0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SaadTech360.pythonanywhere.com']
 
 
 # Application definition
